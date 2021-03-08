5ad2a4f276d4c6319229f432bd8d9afb5b3c8eb6
TT <tos.10041105@icloud.com>
2021-03-08 10:44:50 +0900

    ログイン保持機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      # チェックが付いている場合のみ有効にする
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが誤っています'
@@ -14,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
