814f07581f8760fb526b6d1f7ab59a3595dcbed6
TT <tos.10041105@icloud.com>
2021-03-09 17:06:27 +0900

    料理編集ページ作成

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,4 +1,6 @@
 class DishesController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @dish = Dish.new
@@ -18,9 +20,28 @@ class DishesController < ApplicationController
     @dish = Dish.find(params[:id])
   end
 
+  def edit
+    @dish = Dish.find(params[:id])
+  end
+
+  def update
+    @dish = Dish.find(params[:id])
+    if @dish.update_attributes(dish_params)
+      flash[:success] = "料理情報が更新されました！"
+      redirect_to @dish
+    else
+      render "edit"
+    end
+  end
 
   private
     def dish_params
       params.require(:dish).permit(:name, :description, :popularity, :reference, :required_time, :portion, :tips, :cook_memo)
+    end
+
+    def correct_user
+      # 現在のユーザーが更新対象の料理を保有しているかどうか確認
+      @dish = current_user.dishes.find_by(id: params[:id])
+      redirect_to root_url if @dish.nil?
     end
 end
