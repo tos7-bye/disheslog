45256520215bbd8edca3411be236610105028934
TT <tos.10041105@icloud.com>
2021-03-11 22:44:36 +0900

    フォロー、フォロワー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_many :dishes, dependent: :destroy
+  # 能動的なフォロー
+  has_many :active_relationships, class_name: "Relationship",
+  foreign_key: "follower_id",
+  dependent: :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  # 受動的なフォロー
+  has_many :passive_relationships, class_name: "Relationship",
+  foreign_key: "followed_id",
+  dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token
 
@@ -47,6 +57,26 @@ class User < ApplicationRecord
   # フィード一覧を取得
   def feed
     Dish.where("user_id = ?", id)
+  end
+
+    # ユーザーをフォローする
+    def follow(other_user)
+      following << other_user
+    end
+
+    # ユーザーをフォロー解除する
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # 現在のユーザーがフォローしてたらtrueを返す
+    def following?(other_user)
+      following.include?(other_user)
+    end
+
+  # 現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
 
   private
