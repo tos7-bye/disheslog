be16f65647c7e85b486173dcbbdf1fb0869e1a09
TT <tos.10041105@icloud.com>
2021-03-15 11:05:28 +0900

    レイアウトテスト実装

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-# 全体のレイアウトで、JSファイルをまとめて読み込む
 Rails.application.config.assets.precompile += %w(
   animate.css icomoon.css bootstrap.css flexslider.css \
   style.css magnific-popup.css modernizr-2.6.2.min.js \
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,33 +8,14 @@ RSpec.describe "Users", type: :system do
   let!(:other_dish) { create(:dish, user: other_user) }
 
   describe "ユーザー一覧ページ" do
-    context "管理者ユーザーの場合" do
-      it "ぺージネーション、自分以外のユーザーの削除ボタンが表示されること" do
-        create_list(:user, 30)
-        login_for_system(admin_user)
-        visit users_path
-        expect(page).to have_css "div.pagination"
-        User.paginate(page: 1).each do |u|
-          expect(page).to have_link u.name, href: user_path(u)
-          expect(page).to have_content "#{u.name} | 削除" unless u == admin_user
-        end
-      end
-    end
-
-    context "管理者ユーザー以外の場合" do
-      it "ぺージネーション、自分のアカウントのみ削除ボタンが表示されること" do
-        create_list(:user, 30)
-        login_for_system(user)
-        visit users_path
-        expect(page).to have_css "div.pagination"
-        User.paginate(page: 1).each do |u|
-          expect(page).to have_link u.name, href: user_path(u)
-          if u == user
-            expect(page).to have_content "#{u.name} | 削除"
-          else
-            expect(page).not_to have_content "#{u.name} | 削除"
-          end
-        end
+    it "ぺージネーション、自分以外のユーザーの削除ボタンが表示されること" do
+      create_list(:user, 31)
+      login_for_system(admin_user)
+      visit users_path
+      expect(page).to have_css "div.pagination"
+      User.paginate(page: 1).each do |u|
+        expect(page).to have_link u.name, href: user_path(u)
+        expect(page).to have_content "削除"
       end
     end
   end
@@ -154,7 +135,6 @@ RSpec.describe "Users", type: :system do
           expect(page).to have_content dish.user.name
           expect(page).to have_content dish.required_time
           expect(page).to have_content "★" * dish.popularity + "☆" * (5 - dish.popularity)
-
         end
       end
 
