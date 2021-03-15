b1325853fe7be07b5c3f1a4af8fef390717cf985
TT <tos.10041105@icloud.com>
2021-03-15 20:34:44 +0900

    aws-s3修正2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,22 +1,14 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
+unless Rails.env.development? || Rails.env.test?
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境の場合はS3へアップロード
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = ['S3_BUCKET'] # バケット名
-    config.fog_public = false
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY'], # アクセスキー
-      aws_secret_access_key: ENV['S3_SECRET_KEY'], # シークレットアクセスキー
-      region: ['S3_REGION'], # リージョン
-      path_style: true
+      aws_access_key_id: ['S3_ACCESS_KEY'],
+      aws_secret_access_key: ['S3_SECRET_KEY'],
+      region: ['S3_REGION']
     }
-  else # 本番環境以外の場合はアプリケーション内にアップロード
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
+
+    config.fog_directory  = ['S3_BUCKET']
+    config.cache_storage = :fog
   end
 end
