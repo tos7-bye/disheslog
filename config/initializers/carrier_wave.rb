fa35a862faf5fd7196da3f0bb98038458e852df1
TT <tos.10041105@icloud.com>
2021-03-15 21:38:49 +0900

    AWS-S3修正5

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,8 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'dishlog-toshi'
+  config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['S3_ACCESS_KEY'],
